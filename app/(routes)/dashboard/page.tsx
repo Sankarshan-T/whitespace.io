e7da009e046ca1b23a112a5dfc03fa31e86ca17e
CoolCream <mahasankarshant@gmail.com>
2026-04-19 20:41:07 +0530

    cconvex data set

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { api } from '@/convex/_generated/api';
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import { useConvex, useMutation, useQuery } from 'convex/react';
+import { useEffect } from 'react'
 
 function DashboardPage() {
+    const convex = useConvex();
+    const { user }: any = useKindeBrowserClient();
+    const createUser = useMutation(api.user.createUser);
+
+    useEffect(() => {
+        if (user) {
+            checkUser();
+        }
+    }, [user]);
+
+    const checkUser = async () => {
+        const result = await convex.query(api.user.getUser, { email: user?.email });
+        if (!result?.length) {
+            createUser({
+                name: user.given_name ?? "Anonymous",
+                email: user.email,
+                image: user.picture ?? "",
+            }).then((resp) => {
+                console.log("User created:", resp);
+            });
+        }
+    }
+
     return (
-        <div>DashboardPage</div>
+        <div>
+            DashboardPage
+            <Button>
+                <LogoutLink>Logout</LogoutLink>
+            </Button>
+        </div>
     )
 }
 
