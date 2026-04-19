525443cea8da50d2375edbc0f3e0970d10bff832
CoolCream <mahasankarshant@gmail.com>
2026-04-19 17:24:18 +0530

    pages and login system

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,4 +1,7 @@
+import { Button } from '@/components/ui/button'
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs'
 import Image from 'next/image'
+import { env } from 'process'
 import React from 'react'
 
 function Navbar() {
@@ -26,15 +29,18 @@ function Navbar() {
 
                     <div className="flex items-center gap-4">
                         <div className="sm:flex sm:gap-4">
-                            <a className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-primary-foreground transition hover:scale-105" href="#">
-                                Login
-                            </a>
+                            <Button size={"lg"} className='py-3 px-4 hover:scale-105'>
+                                <LoginLink postLoginRedirectURL={env.KINDE_POST_LOGIN_REDIRECT_URL}>Login</LoginLink>
+                            </Button>
+                            <Button size={"lg"} variant={"outline"} className='py-3 px-4 hover:scale-105'>
+                                <RegisterLink postLoginRedirectURL={env.KINDE_POST_LOGIN_REDIRECT_URL}>Register</RegisterLink>
+                            </Button>
                         </div>
 
                         <button className="block rounded-sm bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
                             <span className="sr-only">Toggle menu</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="size-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                <path strokeLinecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
                             </svg>
                         </button>
                     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import Image from "next/image";
+"use client";
+
 import Navbar from "./_components/Navbar";
 import Hero from "./_components/Hero";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 export default function Home() {
+  const { user } = useKindeBrowserClient();
+
   return (
     <div>
       <Navbar />
